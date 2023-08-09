8b0f92059717de6f23e9d72c9316a6fca93a2023
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-09 17:05:20 +0300

    Send logs to Telegram to admin user

--- homework_bot.py
+++ homework_bot.py
@@ -1,9 +1,22 @@
 import time
 import telegram
 import requests
-from textwrap import dedent
+import logging
+import traceback
 
+from textwrap import dedent
 from environs import Env
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def get_updates(api_token, timestamp=None):
@@ -27,8 +40,9 @@ def main():
     tg_bot_token = env('TG_BOT_TOKEN')
     tg_chat_id = env('TG_CHAT_ID')
     bot = telegram.Bot(token=tg_bot_token)
+    bot.logger.addHandler(TelegramLogsHandler(bot, tg_chat_id))
+    bot.logger.warning('Бот успешно запущен!')
     timestamp = None
-
     while True:
         try:
             review = get_updates(api_token, timestamp)
